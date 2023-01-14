9d1cf86494cee506f717385c0da5dfa977cbea84
JaWitold <witekkaras00@gmail.com>
2023-01-14 16:29:31 +0100

    Writing a Basic Renderer in OpenGL

--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -1,9 +1,20 @@
 #pragma once
 #include <GL/glew.h>
 
+#include "IndexBuffer.h"
+#include "Shader.h"
+#include "VertexArray.h"
+
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
 void GLClearError();
 
 bool GLLogCall(const char* function, const char* file, const int line);
+
+class Renderer
+{
+public:
+	inline void Clear() const {GLCall(glClear(GL_COLOR_BUFFER_BIT));};
+	void Draw(const VertexArray& vertex_array, const IndexBuffer& index_buffer, const Shader& shader) const;
+};
